38d7eb605bc9f12c39a001a5b8322afb61df1693
Titu295 <asatkarsarvesh39+2@gmail.com>
2024-10-20 23:02:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,7 +67,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Titu_Movie") # ho
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://titu-movie.blogspot.com/p/redirect-to-telegram-bot.html") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://titu-movie.blogspot.com/p/redirect-to-telegram.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
